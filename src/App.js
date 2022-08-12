0c7b98d13aeebd7d6af0e6dff8759fdbcf65bec6
satyasridevi-190219 <74642263+satyasridevi-190219@users.noreply.github.com>
2022-08-12 11:22:23 +0100

    committing the files and imcompleting the line graph

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { BrowserRouter, Route,  Routes } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
+import CoinPage from "./Pages/CoinPage";
+import HomePage from "./Pages/HomePage";
+import LoginComponent from "./auth/login";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Routes>
+          <Route exact path="/home" element={<HomePage /> } />
+          <Route exact path="/coins/:id" element={<CoinPage />} />
+          <Route exact path="/" element={<LoginComponent/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
